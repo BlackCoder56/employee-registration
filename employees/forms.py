276239f69a4efe15d170eed1346b75f2ee529191
Elly <blackcoda56@gmail.com>
2023-09-12 13:28:59 +0300

    Version-04

--- employees/forms.py
+++ employees/forms.py
@@ -6,3 +6,12 @@ class EmployeeForm(forms.ModelForm):
     class Meta:
         model = Employee
         fields = ('full_name', 'mobile', 'emp_code', 'position',)
+        labels = {
+            'full_name': 'Full Name',
+            'emp_code': 'EMP. Code'
+        }
+        
+    def __init__(self, *args, **kwargs):
+        super(EmployeeForm, self).__init__(*args, **kwargs)
+        self.fields['position'].empty_label = 'Select Position'
+        self.fields['emp_code'].required = False
